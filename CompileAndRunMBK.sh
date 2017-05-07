672bda4503ace45cdb96e15dbe0302f271bf82df
DhavalNiphade <DhavalNiphade@github.com>
2017-05-07 10:52:06 -0400

    Initial Commit

---
+++ CompileAndRunMBK.sh
@@ -0,0 +1,17 @@
+cd $HARP_ROOT_DIR
+mvn clean package
+
+cd $HARP_ROOT_DIR/harp-tutorial-app
+cp target/harp-tutorial-app.1.0.SNAPSHOT.jar $HADOOP_HOME
+
+cd $HADOOP_HOME
+
+hadoop jar harp-tutorial-app.jar edu.iu.kmeans.common.KmeansMapCollective 1000 10 10 2 10 /kmeans /tmp/kmeans allreduce
+hadoop jar harp-tutorial-app.jar edu.iu.kmeans.common.KmeansMapCollective 1000 10 10 2 10 /kmeans /tmp/kmeans allreduce
+
+#hdfs dfs -ls /
+#hdfs dfs -cat /kmeans/centroids/*
+
+
+
+
